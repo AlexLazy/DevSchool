a6b09759c975c97bc67ad14fa9a889e8cec3f2ea
Alexey Cherepanov <alexlazydev@gmail.com>
2016-04-01 19:23:31 +0300

    fix

--- Lesson_7_Cookies_and_Files/dz7_2/functions.php
+++ Lesson_7_Cookies_and_Files/dz7_2/functions.php
@@ -3,16 +3,32 @@
     function parseForm () {//Собирает данные из массива пост в файл posts.php при отправке формы
         
         if(!file_get_contents('posts.php')) {
-            if (is_array($_POST) && $_POST != '') {
-                $post[] = $_POST;
+            if ($_POST['submit'] === 'Отправить' && is_array($_POST) && $_POST != '') {
+                $post[time()] = $_POST;
                 $post = serialize($post);
                 file_put_contents('posts.php', $post);
             }
         }else{
             $posts = file_get_contents('posts.php');
             $posts = unserialize($posts);
-                if(is_array($_POST) && $_POST != '' && end($posts) !== $_POST) {
-                $posts[] = $_POST;
+            if($_POST['submit'] === 'Отправить' && is_array($_POST) && $_POST != '' && end($posts) !== $_POST) {
+                $posts[time()] = $_POST;
+                $posts = serialize($posts);
+                file_put_contents('posts.php', $posts);
+            }
+        }
+        
+    }
+
+    function editeForm () {//Сохраняет отредактированное объявление в файл posts.php
+        
+        if(!file_get_contents('posts.php')) {
+            exit;
+        }else{
+            if($_POST['submit'] === 'Сохранить' && is_array($_POST) && $_POST != '') {
+                $posts = file_get_contents('posts.php');
+                $posts = unserialize($posts);
+                $posts[$_GET['edit']] = $_POST;
                 $posts = serialize($posts);
                 file_put_contents('posts.php', $posts);
             }
@@ -20,50 +36,67 @@
         
     }
     
-    function showSelect($array, $name, $show = '') {//Выводит селекты формы
+    function showSelect($array, $name) {//Выводит селекты формы
         
         $posts = file_get_contents('posts.php');
         $posts = unserialize($posts);
-        $session = $posts[$_GET['post_id']];
+        if(isset($_GET['post_id'])) {
+            $session = $posts[$_GET['post_id']];
+        }elseif(isset($_GET['edit'])) {
+            $session = $posts[$_GET['edit']];
+        }
+        
         
         foreach ($array as $key => $value) {
             if (is_array($value)) {
                 echo "<optgroup label='" . $key . "'>";
                 foreach ($value as $key => $val) {
-                    ($show == 'show' && $key == $session[$name]) ? $selected = 'selected' : $selected = '';
+                    ($key == $session[$name]) ? $selected = 'selected' : $selected = '';
                     echo "<option value='" . $key . "' " . $selected . ">" . $val . "</option>";
                 }
                 echo "</optgroup>";
             }else{
-                ($show == 'show' && $key == $session[$name]) ? $selected = 'selected' : $selected = '';
+                ($key == $session[$name]) ? $selected = 'selected' : $selected = '';
                 echo "<option value='" . $key . "' " . $selected . ">" . $value . "</option>";
             }
         }
         
     }
 
-    
-    function showInput($val) {//Выводит инпуты со значениями из файлa posts.php
+    function showInput($val) {//Выводит инпуты
         
-        global $private, $company;
-        
-        if(!file_get_contents('posts.php')) {
+        if(isset($_GET['post_id']) || isset($_GET['edit'])) {
+            if(!file_get_contents('posts.php')) {
             exit;
-        }else{
-            $posts = file_get_contents('posts.php');
-            $posts = unserialize($posts);
-            if (isset($posts[$_GET['post_id']][$val])) {//инпут
-                $value = $posts[$_GET['post_id']][$val];
+            }else{
+                $posts = file_get_contents('posts.php');
+                $posts = unserialize($posts);
+                
+                if (isset($_GET['post_id']) && isset($posts[$_GET['post_id']][$val])) {
+                    $value = $posts[$_GET['post_id']][$val];  
+                }elseif(isset($_GET['edit']) && isset($posts[$_GET['edit']][$val])) {
+                    $value = $posts[$_GET['edit']][$val];
+                }
+                
+                if ($val == 'private' && $value == '1') {
+                    echo 'checked';
+                    //Очень стыдно за штуку что ниже)))
+                }elseif($val == 'company' && ((isset($_GET['edit']) && $posts[$_GET['edit']]['private'] == '0') || (isset($_GET['post_id']) && $posts[$_GET['post_id']]['private'] == '0'))) {
+                    echo 'checked';
+                }elseif($val == 'allow_mails' && isset($value)) {
+                    echo 'checked';
+                }elseif(isset($value)) {
+                    echo strip_tags($value);
+                }
             }
-            if ($val == 'private') {                    //радио-баттон и чекбокс
-                ($value > 0) ? $private = 'checked' : $company = 'checked';
-            } elseif ($val == 'allow_mails' && isset($value)) {
+        }else{
+            if($val == 'private') {
                 echo 'checked';
-            } elseif (isset($value)) {
-                echo strip_tags($value);
+            }elseif($val == 'price' && !isset($value)) {
+                echo '0';
             }
         }
-        
+  
     }
     
     function showPost() {//Выводит объявления
@@ -73,34 +106,74 @@
         }else{
             $posts = file_get_contents('posts.php');
             $posts = unserialize($posts);
-            foreach ($posts as $key => $value) {
-                $val = $posts;
-                echo "<div class='panel panel-success'>";
-                echo "<div class='panel-heading'><a href='index.php?post_id=" .
-                       strip_tags($key) . "'>" . strip_tags($val[$key]['title']) . "</a></div>";
-                echo "<div class='panel-body'>" . strip_tags($val[$key]['price']) . " руб | " . 
-                       strip_tags($val[$key]['seller_name']) . " | <a href='?id=" .
-                       strip_tags($key) . "'>Удалить</a></div>";
-                echo "</div>";
+            if(isset($posts)) {
+                foreach ($posts as $key => $value) {
+                    $val = $posts;
+                    echo "<div class='panel panel-success'>";
+                    echo "<div class='panel-heading'><a href='index.php?post_id=" .
+                           strip_tags($key) . "'>" . 
+                           strip_tags($val[$key]['title']) . "</a></div>";
+                    echo "<div class='panel-body'>" . 
+                           strip_tags($val[$key]['price']) . " руб | " . 
+                           strip_tags($val[$key]['seller_name']) . " | <a href='?id=" .
+                           strip_tags($key) . "'>Удалить</a>" . " | <a href='?edit=" .
+                           strip_tags($key) . "'>Редактировать</a></div>";
+                    echo "</div>";
+                }
             }
         } 
         
     }
 
-    function deletePost() {//Удаляет из файлa posts.php объявление
+    function deletePost($var) {//Удаляет из файлa posts.php объявление
         
         global $host;
-        
         if(!file_get_contents('posts.php')) {
             exit;
         }else{
             $posts = file_get_contents('posts.php');
             $posts = unserialize($posts);
-            unset($posts[$_GET['id']]);
+            if(isset($var)) {
+                unset($posts[$var]);
+            }else{
+                unset($posts);
+                $posts = [];
+            }
             $posts = serialize($posts);
             file_put_contents('posts.php', $posts);
             header("Location: http://$host");
             exit;
+        }
+        
+    }
+    
+    //Кнопки форм
+    function formBtnMain() {
+        
+        echo '<input type="submit" value="Отправить" id="form_submit"'
+            . ' name="submit" class="btn btn-primary"><a class="btn btn-danger"'
+            . ' href="?id=-1">Удалить все объявления</a>';
+        
+    }
+    
+    function formBtnCompleted() {
+        
+        global $host;
+        
+        echo '<a href="http://'. $host . '" class="btn btn-primary">Назад</a>';
+        
+    }
+    
+    function formBtnEdit() {
+        
+        global $host;
+        
+        echo '<a href="http://' . $host . '" class="btn btn-primary">Назад</a>'
+            . '<input type="submit" value="Сохранить" id="form_submit"'
+            . ' name="submit" class="btn btn-success">';
+        if(isset($_POST['submit'])) {
+            echo '<div class="panel panel-success" id="edit"><div class="panel-body">
+                    Изменения сохранены</div></div>';
         }
         
     }
--- Lesson_7_Cookies_and_Files/dz7_2/index.php
+++ Lesson_7_Cookies_and_Files/dz7_2/index.php
@@ -1,34 +1,45 @@
 <?php
-    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    error_reporting(E_ALL | E_STRICT);
     ini_set('display_errors', 1);
 
     $host  = $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-    $private = '';
-    $company = '';
 
     require( dirname( __FILE__ ) . '/date.php' );//Подключение файла данных
     require( dirname( __FILE__ ) . '/functions.php' );//Подключение функций
    
     
-    if (isset($_GET['id'])) {
-        $_GET['id'] = (int)$_GET['id'];
-        deletePost();
+    if (isset($_GET['id']) && filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT) >= -1) {
+        ($_GET['id'] == -1) ? deletePost() : deletePost($_GET['id']);
     }elseif(isset($_POST['submit'])) {
         parseForm();
-//        header("Location: http://$host");
+        editeForm();
     }
     
     require( dirname( __FILE__ ) . '/header.html' );//Подключение шапки
     
-    if(isset($_GET['post_id'])) {
-        $_GET['post_id'] = (int)$_GET['post_id'];
-        require( dirname( __FILE__ ) . '/single_post.html' );//Выводит заполненную форму
-       
+    
+    if(isset($_GET['post_id']) && filter_input(INPUT_GET, 'post_id', FILTER_VALIDATE_INT) > 0) {
+        $posts = file_get_contents('posts.php');
+        $posts = unserialize($posts);
+        
+        if(isset($posts[$_GET['post_id']])) {
+            require( dirname( __FILE__ ) . '/form.html' );//Выводит форму
+            formBtnCompleted();
+        }else{
+            echo 'Объявление отсутствует';
+            exit;
+        }
+        
+    }elseif(isset($_GET['edit']) && filter_input(INPUT_GET, 'edit', FILTER_VALIDATE_INT) > 0) {
+        require( dirname( __FILE__ ) . '/form.html' );//Выводит форму
+        formBtnEdit();
     }else{
-        require( dirname( __FILE__ ) . '/form.html' );//Выводит пустую форму
+        require( dirname( __FILE__ ) . '/form.html' );//Выводит форму
+        formBtnMain();
         showPost();
     }
-
+    
+    
     require( dirname( __FILE__ ) . '/footer.html' );//Подключение подвала
     
 ?>
