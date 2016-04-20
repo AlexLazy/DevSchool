d70c1f42494067692bffbbadc16e22135152d6dd
Alexey Cherepanov <alexlazydev@gmail.com>
2016-04-20 22:20:02 +0300

    first

---
+++ Lesson_11_OOP/smarty/templates/ad.tpl
@@ -0,0 +1,17 @@
+<div class="col-md-6">
+{if $ad->private == '1'}
+    <div class="panel panel-success">
+{else}
+    <div class="panel panel-warning">
+{/if}
+        <div class="panel-heading">
+            <a href="index.php?ads={$ad->getId()}">{$ad->getTitle()}</a>
+            <a href="?delete={$ad->getId()}" class='close'>×</a>
+        </div>
+        <div class="panel-body">
+            {$ad->getPrice()}руб |
+            {$ad->getName()} |
+            <a href="?edit={$ad->getId()}">Редактировать</a>
+        </div>
+    </div>
+</div>
---
+++ Lesson_11_OOP/smarty/templates_c/%%66^66F^66F6E0D5%%ad.tpl.php
@@ -0,0 +1,25 @@
+<?php /* Smarty version 2.6.25-dev, created on 2016-04-20 21:34:34
+         compiled from ad.tpl */ ?>
+<div class="col-md-6">
+<?php if ($this->_tpl_vars['ad']->private == '1'): ?>
+    <div class="panel panel-success">
+<?php else: ?>
+    <div class="panel panel-warning">
+<?php endif; ?>
+        <div class="panel-heading">
+            <a href="index.php?ads=<?php echo $this->_tpl_vars['ad']->getId(); ?>
+"><?php echo $this->_tpl_vars['ad']->getTitle(); ?>
+</a>
+            <a href="?delete=<?php echo $this->_tpl_vars['ad']->getId(); ?>
+" class='close'>×</a>
+        </div>
+        <div class="panel-body">
+            <?php echo $this->_tpl_vars['ad']->getPrice(); ?>
+руб |
+            <?php echo $this->_tpl_vars['ad']->getName(); ?>
+ |
+            <a href="?edit=<?php echo $this->_tpl_vars['ad']->getId(); ?>
+">Редактировать</a>
+        </div>
+    </div>
+</div>
