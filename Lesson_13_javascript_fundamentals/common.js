5c13c28c1c571ee1235945549a3202f1224cd76f
Alexey Cherepanov <alexlazydev@gmail.com>
2016-04-30 11:28:24 +0300

    add display method to books obj

--- Lesson_13_javascript_fundamentals/common.js
+++ Lesson_13_javascript_fundamentals/common.js
@@ -22,7 +22,9 @@ function three()
         author: 'Mett',
         pages: 200
     };
-    console.log('Недавно я прочитал книгу '+book.title+', написанную автором '+book.author+', я осилил все '+book.pages+' страниц, мне она очень понравилась.');
+    console.log('Недавно я прочитал книгу '+book.title+', написанную автором '+
+            book.author+', я осилил все '+book.pages+
+            ' страниц, мне она очень понравилась.');
 }
 
 function four()
@@ -41,9 +43,16 @@ function four()
     
     var books = {
         book1: book1,
-        book2: book2
+        book2: book2,
+        display: function()
+        {
+            console.log('Недавно я прочитал книги '+this.book1.title+' и '+
+                    this.book2.title+', написанные соответственно авторами '+
+                    this.book1.author+' и '+this.book2.author+
+            ', я осилил в сумме '+(this.book1.pages+this.book2.pages)+
+            ' страниц, не ожидал от себя подобного.');
+        }
     };
-    console.log('Недавно я прочитал книги '+books.book1.title+' и '+books.book2.title+', написанные соответственно авторами '+books.book1.author+' и '+books.book2.author+
-            ', я осилил в сумме '+(books.book1.pages+books.book2.pages)+' страниц, не ожидал от себя подобного.');
+    return books.display();
 }
 one(), two(), three(), four();
