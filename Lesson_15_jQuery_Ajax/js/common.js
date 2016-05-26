1b79211b7bdaa30b7e06243e52242113d7584665
Alexey Cherepanov <alexlazydev@gmail.com>
2016-05-26 12:26:38 +0300

    Добавил очистку формы при удалении объявления

--- Lesson_15_jQuery_Ajax/js/common.js
+++ Lesson_15_jQuery_Ajax/js/common.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
     $('.panel .close').on('click', function () {//удаление выбранного объявления
         var ads = $(this).closest('.panel'),
             id = ads.children('i').html();
-            
+           
         $.get('index.php?delete=' + id, function(){
             ads.fadeOut('slow',function(){
                 $(this).remove();
             })
+            if($('input[name="id"]').val() === id){
+                $(':input, form')
+                    .find('input[name=private]:first').attr('checked','checked').end()
+                    .not(':button, :submit, :reset, :hidden, #fld_price, input[name=private]:first')
+                    .val('')
+                    .removeAttr('selected')
+                    .removeAttr('checked');
+            }
         });
     });
     
